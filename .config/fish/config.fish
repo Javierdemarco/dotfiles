b270722dc0708cab2a7d01cda8c232733325abb1
javierdemarco <javierdemarcoo@gmail.com>
2025-04-03 19:50:46 +0200

    NNN config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,8 +17,8 @@
 set -Ux NNN_BMS "p:~/Documents/Git/;d:~/Downloads/;h:~/"
 set -Ux NNN_ARCHIVE "\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)\$"
 set -Ux NNN_FIFO /tmp/nnn.fifo
-set -Ux NNN_PLUG "p:preview-tui;P:preview-tabbed"
-alias nnn="nnn -Hi"
+set -Ux NNN_PLUG "p:preview-tui"
+alias nnn="nnn -Hi -P p"
 
 # Use background tasks for some heavy actions (example: delete in background)
 # set -x NNN_BG_DEL 1 # Enable background deletion (useful for large directories)
