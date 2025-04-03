707fe7368272f548081d99425c3b2a844e7dc0ed
javierdemarco <javierdemarcoo@gmail.com>
2025-04-03 21:47:00 +0200

    nnn

---
+++ .config/nvim/lua/plugins/nnn-inside.lua
@@ -0,0 +1,23 @@
+return {
+    "luukvbaal/nnn.nvim",
+    config = function()
+        require("nnn").setup({
+            explorer = {
+                cmd = "nnn -Hi", -- command override (-F1 flag is implied, -a flag is invalid!)
+                session = "local", -- or "global" / "local" / "shared"
+            },
+            picker = {
+                cmd = "nnn -Hi -Pp", -- command override (-p flag is implied)
+                session = "local", -- or "global" / "local" / "shared"
+            },
+            replace_netrw = "explorer", -- or "explorer" / "picker"
+            buflisted = false, -- whether or not nnn buffers show up in the bufferlist
+            quitcd = nil, -- or "cd" / tcd" / "lcd", command to run on quitcd file if found
+            offset = true,
+        })
+    end,
+    keys = {
+        { "<leader>Fn", "<cmd>NnnExplorer %:p:h<cr>", desc = "NnnExplorer" },
+        { "<leader>FN", "<cmd>NnnPicker %:p:h<cr>", desc = "NnnPicker" },
+    },
+}
