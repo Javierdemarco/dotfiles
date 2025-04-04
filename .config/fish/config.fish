b909d84f297d22a0683faa2b517149bc17ed51dd
javierdemarco <javierdemarcoo@gmail.com>
2025-04-04 10:43:51 +0200

    Some env variables and aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,24 +1,18 @@
-# nnn Configuration File
+# ENV Vars Config
+set -Ux EDITOR nvim
+set -Ux VISUAL nvim
+set -Ux PAGER ov
+set -Ux MANPAGER ov
 
-# Enable file previews with the 'p' key (requires a preview plugin)
-#set -x NNN_PLUG preview-tui # Load the preview plugin
-
-# Keybinding Customizations:
-# Here we modify keybindings to match some preferred actions
-# You can look up the default key bindings in the `nnn` manual or `nnn` GitHub page
-
-#set -x NNN_BMS "b:~/Documents,b:~/Downloads" # Set bookmarks (using 'b' to jump to directories)
-#set -x NNN_OPTS --no-mouse # Disable mouse support (useful for keyboard-only navigation)
-
-# Setup Preview Plugin for File Previews
-# set -x NNN_PLUG  # Enables file previews on 'p' key press
-
-# Show hidden files by default
-set -Ux NNN_BMS "p:~/Documents/Git/;d:~/Downloads/;h:~/"
+# NNN Config
+set -Ux NNN_BMS "p:~/Documents/Git/;d:~/Downloads/;h:~/;f:~/.config/fish/"
 set -Ux NNN_ARCHIVE "\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)\$"
 set -Ux NNN_FIFO /tmp/nnn.fifo
 set -Ux NNN_PLUG "p:preview-tui"
 alias nn="nnn -Hi -P p"
+
+## Alias
+alias lg="lazygit"
 
 # Use background tasks for some heavy actions (example: delete in background)
 # set -x NNN_BG_DEL 1 # Enable background deletion (useful for large directories)
